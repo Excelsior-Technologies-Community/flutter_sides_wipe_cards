fb501fabec91c922bc3719f04dc377cccafd7ad3
Chavda Harsh <chavdaharsh216@gmail.com>
2026-09-02 11:26:53 +0530

    exports file

--- lib/flutter_sideswipe_cards.dart
+++ lib/flutter_sideswipe_cards.dart
@@ -1,5 +1,5 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library;
+
+export 'src/side_swipe_cards.dart';
+export 'src/side_swipe_controller.dart';
+export 'src/side_swipe_direction.dart';
